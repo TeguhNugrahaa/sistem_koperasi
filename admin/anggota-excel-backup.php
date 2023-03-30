9ed53b9ea9e7e0b3c865fecade023c851a09b058
unknown <teguhnugraha065@gmail.com>
2023-03-30 09:38:04 +0700

    commit

--- admin/anggota-excel-backup.php
+++ admin/anggota-excel-backup.php
@@ -1,63 +1,76 @@
 <?php
-header("Content-type: application/vnd-ms-excel");
-header("Content-Disposition: attachment; filename=Anggota-Excel.xls");
-?>
-
-
-<table class="table table-bordered table-striped" style="border: 1; border-collapse: collapse;">
-    <thead>
-        <tr>
-            <th width="1%">NO</th>
-            <th width="1%">NAMA LENGKAP</th>
-            <th class="text-center">ALAMAT</th>
-            <th class="text-center">TEMPAT LAHIR</th>
-            <th class="text-center">TANGGAL LAHIR</th>
-            <th class="text-center">NO. HANDPHONE</th>
-            <th class="text-center">EMAIL</th>
-            <th class="text-center">PEKERJAAN</th>
-            <th class="text-center">JUMLAH TANGGUNGAN SAAT INI</th>
-            <th class="text-center">FOTO KTP</th>
-            <th class="text-center">FOTO NPWP ANGGOTA</th>
-            <th class="text-center">UPLOAD BUKTI PEMBAYARAN</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        include '../koneksi.php';
-        $no = 1;
-        $data = mysqli_query($koneksi, "SELECT * FROM anggota");
-        while ($d = mysqli_fetch_array($data)) {
-        ?>
-            <tr style="text-align: center;">
-                <td class="text-center"><?php echo $no++; ?></td>
-                <td class="text-center"><?php echo $d['anggota_nama']; ?></td>
-                <td class="text-center"><?php echo $d['anggota_alamat']; ?></td>
-                <td class="text-center"><?php echo $d['anggota_tempat']; ?></td>
-                <td class="text-center"><?php echo $d['anggota_tanggal']; ?></td>
-                <td class="text-center"><?php echo $d['anggota_handphone']; ?></td>
-                <td class="text-center"><?php echo $d['anggota_email']; ?></td>
-                <td class="text-center"><?php echo $d['anggota_pekerjaan']; ?></td>
-                <td class="text-center"><?php echo $d['anggota_tanggungan']; ?></td>
+header('Content-Type: application/vnd.ms-excel');
+header('Content-Disposition: attachment;filename="file_excel.xls"');
 
 
 
-                <td class="text-center"><img src="../gambar/user/upload_ktp/<?php echo $d['anggota_ktp']; ?>" width="50px"></td>
-                <td class="text-center"><img src="../gambar/user/upload_npwp/<?php echo $d['anggota_npwp']; ?>" width="50px"></td>
-                <td class="text-center"><img src="../gambar/user/upload_bukti/<?php echo $d['anggota_bukti']; ?>" width="50px"></td>
+echo '<table class="table table-bordered table-striped" style="border: 1; border-collapse: collapse;">';
+echo '  <thead>';
+echo '     <tr>';
+echo '           <th width="1%">NO</th>';
+echo '           <th width="1%">NAMA LENGKAP</th>';
+echo '        <th class="text-center">ALAMAT</th>';
+echo '   <th class="text-center">TEMPAT LAHIR</th>';
+echo '<th class="text-center">TANGGAL LAHIR</th>';
+echo '            <th class="text-center">NO. HANDPHONE</th>';
+echo '            <th class="text-center">EMAIL</th>';
+echo '            <th class="text-center">PEKERJAAN</th>';
+echo '            <th class="text-center">JUMLAH TANGGUNGAN SAAT INI</th>';
+echo '            <th class="text-center">FOTO KTP</th>';
+echo '            <th class="text-center">FOTO NPWP ANGGOTA</th>';
+echo '            <th class="text-center">UPLOAD BUKTI PEMBAYARAN</th>';
+echo '        </tr>';
+echo '    </thead>';
+echo '    <tbody>';
+include '../koneksi.php';
+$no = 1;
+$data = mysqli_query($koneksi, "SELECT * FROM anggota");
 
-            </tr>
-        <?php
-        }
-        ?>
-    </tbody>
-</table>
-</div>
-</div>
+while ($d = mysqli_fetch_array($data)) {
 
-</div>
-</section>
-</div>
-</section>
+    $baseUrl = 'http://localhost/sistem_koperasi/';
+    $npwp = $baseUrl . 'gambar/user/upload_npwp/' . $d['anggota_npwp'];
+    $ktp = $baseUrl . 'gambar/user/upload_ktp/' . $d['anggota_ktp'];
+    $bukti = $baseUrl . 'gambar/user/upload_bukti/' . $d['anggota_bukti'];
+    $width = 100;
+    $imagenpwp = '<img src="' . $npwp . '" width="' . $width . '" />';
+    $imagektp = '<img src="' . $ktp . '" width="' . $width . '" />';
+    $imagebukti = '<img src="' . $bukti . '" width="' . $width . '" />';
 
-</div>
-<?php include 'footer.php'; ?>
+
+    echo '            <tr style="text-align: center;">';
+    echo '                <td class="text-center">' . $no++ . '</td>';
+    echo '                <td class="text-center">' . $d['anggota_nama'] . '</td>';
+    echo '                <td class="text-center">' . $d['anggota_alamat'] . '</td>';
+    echo '                <td class="text-center">' . $d['anggota_tempat'] . '</td>';
+    echo '                <td class="text-center">' . $d['anggota_tanggal'] . ' </td>';
+    echo '                <td class="text-center">' . $d['anggota_handphone'] . ' </td>';
+    echo '                <td class="text-center">' . $d['anggota_email'] . '</td>';
+    echo '                <td class="text-center">' . $d['anggota_pekerjaan'] . '</td>';
+    echo '                <td class="text-center">' . $d['anggota_tanggungan'] . '</td>';
+
+
+
+    echo '                <td class="text-center">' . $imagektp . '</td>';
+    echo '                <td class="text-center">' . $imagenpwp . '</td>';
+    echo '                <td class="text-center">' . $imagebukti . '</td>';
+
+
+
+    echo '            </tr>';
+}
+
+echo '    </tbody>';
+echo '</table>';
+echo '</div>';
+echo '</div>';
+
+echo '</div>';
+echo '</section>';
+echo '</div>';
+echo '</section>';
+
+echo '</div>';
+
+?>;
+<?php include 'footer.php'; ?>;
--- admin/anggota-excel-backup2.php
+++ admin/anggota-excel-backup2.php
@@ -7,42 +7,43 @@ header("Content-Disposition: attachment; filename=Anggota-Excel.xls");
 <table class="table table-bordered table-striped" style="border: 1; border-collapse: collapse;">
     <thead>
         <tr>
-
-            <th style="border-width:medium;" rowspan="2">NO</th>
-            <th style="border:medium;" rowspan="2">NAMA LENGKAP</th>
-            <th style="border-width:medium;" rowspan="2">ALAMAT</th>
-            <th style="border-width:medium;" rowspan="2">TEMPAT LAHIR</th>
-            <th style="border-width:medium;" rowspan="2">NO HANDPHONE</th>
-            <th style="border-width:medium;" rowspan="2">EMAIL</th>
-            <th style="border-width:medium;" rowspan="2">PEKERJAAN</th>
-            <th style="border-width:medium;" rowspan="2">JUMLAH TANGGUNGAN SAAT INI</th>
-            <th style="border-width:medium;" rowspan="2">FOTO KTP</th>
-            <th style="border-width:medium;" rowspan="2">FOTO NPWP ANGGOTA</th>
-            <th style="border-width:medium;" rowspan="2">UPLOAD BUKTI PEMBAYARAN</th>
+            <th width="1%">NO</th>
+            <th width="1%">NAMA LENGKAP</th>
+            <th class="text-center">ALAMAT</th>
+            <th class="text-center">TEMPAT LAHIR</th>
+            <th class="text-center">TANGGAL LAHIR</th>
+            <th class="text-center">NO. HANDPHONE</th>
+            <th class="text-center">EMAIL</th>
+            <th class="text-center">PEKERJAAN</th>
+            <th class="text-center">JUMLAH TANGGUNGAN SAAT INI</th>
+            <th class="text-center">FOTO KTP</th>
+            <th class="text-center">FOTO NPWP ANGGOTA</th>
+            <th class="text-center">UPLOAD BUKTI PEMBAYARAN</th>
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($anggota as $data) { ?>
-            <tr>
-                <!--td><!?= no->anggota_id ?></td>-->
-                <td><?= $data->anggota_id ?></td>
-                <td style="text-align:center"><?= $data->anggota_nama ?></td>
-                <td style="text-align:center"><?= $data->anggota_alamat ?></td>
-                <td style="text-align:center"><?= $data->anggota_tempat ?></td>
-                <td style="text-align:center"><?= $data->anggota_tanggal ?></td>
-                <td style="text-align:center"><?= $data->anggota_handphone ?></td>
-                <td style="text-align:center"><?= $data->anggota_email ?></td>
-                <td style="text-align:center"><?= $data->anggota_pekerjaan ?></td>
-                <td style="text-align:center"><?= $data->anggota_tanggungan ?></td>
+        <?php
+        include '../koneksi.php';
+        $no = 1;
+        $data = mysqli_query($koneksi, "SELECT * FROM anggota");
+        while ($d = mysqli_fetch_array($data)) {
+        ?>
+            <tr style="text-align: center;">
+                <td class="text-center"><?php echo $no++; ?></td>
+                <td class="text-center"><?php echo $d['anggota_nama']; ?></td>
+                <td class="text-center"><?php echo $d['anggota_alamat']; ?></td>
+                <td class="text-center"><?php echo $d['anggota_tempat']; ?></td>
+                <td class="text-center"><?php echo $d['anggota_tanggal']; ?></td>
+                <td class="text-center"><?php echo $d['anggota_handphone']; ?></td>
+                <td class="text-center"><?php echo $d['anggota_email']; ?></td>
+                <td class="text-center"><?php echo $d['anggota_pekerjaan']; ?></td>
+                <td class="text-center"><?php echo $d['anggota_tanggungan']; ?></td>
 
 
-                <td style="width:135px; height:135px; text-align:center;">
-                    <img src="<?= base_url() . '../gambar/user/upload_ktp/' . $data->anggota_ktp ?>TH.jpg" style="margin:5px; text-align:center; vertical-align:middle;" />
-                    <img src="<?= base_url() . '../gambar/user/upload_npwp/' . $data->anggota_npwp ?>TH.jpg" style="margin:5px; text-align:center; vertical-align:middle;" />
-                    <img src="<?= base_url() . '../gambar/user/upload_bukti/' . $data->anggota_bukti ?>TH.jpg" style="margin:5px; text-align:center; vertical-align:middle;" />
 
-
-                </td>
+                <td class="text-center"><img src="../gambar/user/upload_ktp/<?php echo $d['anggota_ktp']; ?>" width="50px"></td>
+                <td class="text-center"><img src="../gambar/user/upload_npwp/<?php echo $d['anggota_npwp']; ?>" width="50px"></td>
+                <td class="text-center"><img src="../gambar/user/upload_bukti/<?php echo $d['anggota_bukti']; ?>" width="50px"></td>
 
             </tr>
         <?php
